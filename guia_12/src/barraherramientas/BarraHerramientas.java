94c22de0a882335d8159c95f30a8ca667eb29efc
Isac pacheco <isac.pachecolopez@unicolombo.edu.co>
2024-04-24 16:33:19 -0500

    Barras de herramientas

---
+++ guia_12/src/barraherramientas/BarraHerramientas.java
@@ -0,0 +1,21 @@
+
+package barraherramientas;
+
+/**
+ *
+ * @author isac pacheco
+ */
+public class BarraHerramientas {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Principal principal = new Principal();
+        
+        principal.setLocationRelativeTo(null);
+        principal.setVisible(true);
+    }
+    
+}
